e66aaa1abd5fa88c10c68a2308e9d4439a0eafab
VladVorniceanu <vorniceanu.v@gmail.com>
2024-03-11 18:41:13 +0200

    Initial Commit

---
+++ PhotoDex/PhotoDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDexApp.swift
+//  PhotoDex
+//
+//  Created by Vlad Vorniceanu on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
